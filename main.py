00f0c6bf59df6a646d2520c995777935c478af0c
Oleh Kuznetsov <oposteriori+code@protonmail.com>
2024-04-23 22:36:55 +0200

    Use omnivoreQL library instead of raw requests
    Also update requirements

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-import requests
 import os
 import json
 
+from datetime import datetime, timedelta
+from omnivoreql import OmnivoreQL
 from dotenv import load_dotenv
 
 load_dotenv()
 
 OMNIVORE_API_KEY = os.getenv("OMNIVORE_API_KEY")
-OMNIVORE_API_URL = "https://api-prod.omnivore.app/api/graphql"
 
 
 def main():
-    body = "query Viewer { me { id name } }"
-    headers = {"Authorization": OMNIVORE_API_KEY, "content-type": "application/json"}
+    # Setup OmnivoreQL client
+    omnivoreql_client = OmnivoreQL(OMNIVORE_API_KEY)
+    # Get username
+    username = omnivoreql_client.get_profile()["me"]["profile"]["username"]
+    print("Profile:", username)
 
-    response = requests.post(url=OMNIVORE_API_URL, json={"query": body}, headers=headers)
-    print("Response status code: ", response.status_code)
-    if response.status_code == 200:
-        content = json.loads(response.content)
-        print("Response : ", content)
+    # Get posts from subscriptions in the last week (RSS/Newsletter)
+    date_one_week_ago = datetime.now() - timedelta(days=7)
+    query = (
+        f"in:inbox has:subscriptions saved:{date_one_week_ago.strftime('%Y-%m-%d')}..*"
+    )
+    articles = omnivoreql_client.get_articles(
+        query=query, include_content=True, limit=1
+    )
+    print("Articles:", json.dumps(articles, indent=2))
 
 
 if __name__ == "__main__":
